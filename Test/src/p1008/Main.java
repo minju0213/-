18331464f39ed1b808be3dde5c47806d0e93fb71
minju0213 <mjni1214@naver.com>
2022-10-27 15:18:22 +0900

    출력문과 사칙연산 복습

---
+++ Test/src/p1008/Main.java
@@ -0,0 +1,15 @@
+package p1008;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double A = scanner.nextInt();
+		double B = scanner.nextInt();
+		double C = A/B;
+		System.out.println(C);
+	}
+
+}
---
+++ Test/src/p10998/Main.java
@@ -0,0 +1,16 @@
+package p10998;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = A*B;
+		System.out.println(C);
+
+	}
+
+}
